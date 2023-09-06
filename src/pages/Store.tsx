6957dbea59e04a22d875d6ce2220493d4ed2f75f
seod4423 <seod4423@gmail.com>
2023-09-06 21:11:32 +0900

    style: layout 적용

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,31 +1,22 @@
 import BasicTabs from '../components/store/BasicTabs'
-import { Box, CardMedia, Rating, Stack, Typography } from '@mui/material'
+import { Box, CardMedia, Rating, Stack, Typography, styled } from '@mui/material'
 
 export const Store = () => {
   return (
-    <Stack width={'100%'} direction={'column'} marginX={'auto'}>
+    <Stack width={'100%'} direction={'column'} marginX={'auto'} bgcolor={'white'}>
       <Box>
         <CardMedia component="img" image="/src/assets/pizzahut.png" alt="banner" />
       </Box>
-      <Box
-        sx={{
-          width: '100%',
-          height: 100,
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <StyledStoreInfoBox>
         <Typography variant="h4">피자헛 피자 어느 지점</Typography>
         <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
-      </Box>
-      <Box display={'flex'} justifyContent={'center'}>
         <Typography>리뷰 N개</Typography>
-      </Box>
-      <Box sx={{ width: '100%', height: '100%' }}>
-        <BasicTabs />
-      </Box>
+      </StyledStoreInfoBox>
+      <BasicTabs />
     </Stack>
   )
 }
+
+const StyledStoreInfoBox = styled(Stack)`
+  align-items: center;
+`
