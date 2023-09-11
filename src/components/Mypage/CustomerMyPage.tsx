d96a2c2ce5ea1841f0a233ec1131c0228b85ed89
seod4423 <seod4423@gmail.com>
2023-09-11 13:31:32 +0900

    fix: update styles

--- src/components/Mypage/CustomerMyPage.tsx
+++ src/components/Mypage/CustomerMyPage.tsx
@@ -4,27 +4,30 @@ import { ReviewCard } from '../store/ReviewCard'
 export const CustomerMyPage = () => {
   return (
     <Stack justifyContent={'center'} alignItems={'center'} m={'auto'}>
-      <Box bgcolor={'secondary.main'}>
+      <Box>
         CustomerMyPage
-        <Stack p={10} spacing={2} flexDirection={'row'}>
+        <Stack p={1} spacing={2} flexDirection={'row'}>
           <Stack p={4} width={200} flexDirection={'column'} alignItems={'center'} bgcolor={'white'}>
             <Avatar sx={{ width: 100, height: 100, bgcolor: 'primary.main' }}></Avatar>
             <Typography m={2} variant="body1" display={'inline-block'} bgcolor={'primary.light'}>
               ninkname
             </Typography>
           </Stack>
-          <Stack m={0} bgcolor={'white'}>
-            <Typography>Id : id</Typography>
-            <Typography>Phone : phone</Typography>
-            <Typography>Address : address</Typography>
-            <Typography>Detail Address : detail address</Typography>
+          <Stack bgcolor={'white'} flexDirection={'column'} justifyContent={'center'}>
+            <Stack>
+              <Typography>Id : id</Typography>
+              <Typography>Phone : phone</Typography>
+              <Typography>Address : address</Typography>
+              <Typography>Detail Address : detail address</Typography>
+            </Stack>
           </Stack>
         </Stack>
-        <Box p={10}>
+        <Box p={1}>
           review
           <ReviewCard />
           <ReviewCard />
           <ReviewCard />
+          <hr />
           <Card>
             <CardHeader>asdf</CardHeader>
             <CardContent>
