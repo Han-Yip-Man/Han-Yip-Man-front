0ba56102482cb2917bac007cc5efc9253f076c75
seod4423 <seod4423@gmail.com>
2023-09-11 21:35:24 +0900

    fix: update styles

--- src/components/Mypage/CustomerMyPage.tsx
+++ src/components/Mypage/CustomerMyPage.tsx
@@ -1,45 +1,126 @@
-import { Avatar, Box, Card, CardContent, CardHeader, Stack, Typography } from '@mui/material'
+import {
+  Avatar,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography,
+  styled,
+} from '@mui/material'
 import { ReviewCard } from '../store/ReviewCard'
 
 export const CustomerMyPage = () => {
   return (
-    <Stack justifyContent={'center'} alignItems={'center'} m={'auto'}>
-      <Box>
-        CustomerMyPage
-        <Stack p={1} spacing={2} flexDirection={'row'}>
-          <Stack p={4} width={200} flexDirection={'column'} alignItems={'center'} bgcolor={'white'}>
-            <Avatar sx={{ width: 100, height: 100, bgcolor: 'primary.main' }}></Avatar>
-            <Typography m={2} variant="body1" display={'inline-block'} bgcolor={'primary.light'}>
-              ninkname
-            </Typography>
-          </Stack>
+    <MypageWrap>
+      <CustomerInfoTitle variant="h5">내 정보</CustomerInfoTitle>
+      <CustomerInfo>
+        <CustomerInfoInner>
+          <AvatarWarp>
+            <AvatarPaper>
+              <StyledAvatar />
+              <Nickname variant="h5">ninkname</Nickname>
+            </AvatarPaper>
+          </AvatarWarp>
           <Stack bgcolor={'white'} flexDirection={'column'} justifyContent={'center'}>
-            <Stack>
-              <Typography>Id : id</Typography>
-              <Typography>Phone : phone</Typography>
-              <Typography>Address : address</Typography>
-              <Typography>Detail Address : detail address</Typography>
-            </Stack>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <StyledTableCell>
+                    <Typography>ID :</Typography>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <Typography> id</Typography>
+                  </StyledTableCell>
+                </TableRow>
+                <TableRow>
+                  <StyledTableCell>
+                    <Typography>PHONE :</Typography>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <Typography> 010-1234-5678</Typography>
+                  </StyledTableCell>
+                </TableRow>
+                <TableRow>
+                  <StyledTableCell>
+                    <Typography>ADDRESS :</Typography>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <Typography> 서울특별시 강남구 역삼동 837-24</Typography>
+                  </StyledTableCell>
+                </TableRow>
+                <TableRow>
+                  <StyledTableCell>
+                    <Typography>DETAIL ADDRESS :</Typography>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <Typography> 9F</Typography>
+                  </StyledTableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
           </Stack>
-        </Stack>
-        <Box p={1}>
-          review
-          <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
-          <hr />
-          <Card>
-            <CardHeader>asdf</CardHeader>
-            <CardContent>
-              sdfg asdf qwer
-              <Typography>Id : id</Typography>
-              <Typography>Phone : phone</Typography>
-              <Typography>Address : address</Typography>
-              <Typography></Typography>
-            </CardContent>
-          </Card>
-        </Box>
-      </Box>
-    </Stack>
+        </CustomerInfoInner>
+      </CustomerInfo>
+      <CustomerInfoTitle variant="h5">리뷰관리</CustomerInfoTitle>
+      <ReviewPaper>
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
+      </ReviewPaper>
+    </MypageWrap>
   )
 }
+
+const MypageWrap = styled(Stack)`
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  padding-top: 5%;
+`
+
+const CustomerInfoTitle = styled(Typography)`
+  margin-right: 80%;
+  margin-bottom: 20px;
+`
+
+const CustomerInfo = styled(Paper)`
+  width: 100%;
+  margin: 20px 10px 40px 10px;
+`
+
+const CustomerInfoInner = styled(Stack)`
+  padding: 8px;
+  flex-direction: row;
+`
+
+const AvatarWarp = styled(Stack)`
+  width: 200;
+  padding: 32px;
+  flex-direction: column;
+  align-items: center;
+`
+
+const AvatarPaper = styled(Paper)`
+  padding: 16px;
+`
+
+const StyledAvatar = styled(Avatar)`
+  width: 130px;
+  height: 130px;
+`
+
+const Nickname = styled(Typography)`
+  display: inline-box;
+  margin: 16px;
+`
+const StyledTableCell = styled(TableCell)`
+  border-bottom: none;
+`
+
+const ReviewPaper = styled(Paper)`
+  width: 100%;
+  margin: 20px 10px;
+`
