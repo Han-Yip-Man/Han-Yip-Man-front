9bda6a5196955c5150ac6e59a3a0689761ee32bb
swp91 <seongwoo4477@gmail.com>
2023-09-11 20:51:13 +0900

    feat: 회원가입 중간페이지 제작

--- src/router.tsx
+++ src/router.tsx
@@ -12,6 +12,7 @@ import OwnerSignin from './pages/OwnerSignin/OwnerSignin'
 import CustomerSignup from './pages/CustomerSignup/CustomerSignup'
 import CustomerSignin from './pages/CustomerSignin/CustomerSignin'
 import Mypage from './components/Mypage/Mypage/Mypage'
+import SignInSelectionPage from './pages/SignInSelectionPage/SignInSelectionPage'
 
 interface RouterBase {
   id: number // 페이지 아이디 (반복문용 고유값)
@@ -63,57 +64,64 @@ const routerData: RouterElement[] = [
   },
   {
     id: 50,
-    path: '/123',
+    path: '/auth',
     label: 'signlayout',
     element: <SignLayout />,
     withAuth: false,
     children: [
       {
         id: 51,
-        path: '1',
-        label: 'test',
+        path: 'sellersignup',
+        label: 'sellersignup',
         element: <OwnerSignup />,
       },
       {
         id: 52,
-        path: '2',
-        label: 'test',
+        path: 'sellersignin',
+        label: 'sellersignin',
         element: <OwnerSignin />,
       },
       {
         id: 53,
-        path: '3',
-        label: 'test',
+        path: 'usersignup',
+        label: 'usersignup',
         element: <CustomerSignup />,
       },
       {
         id: 54,
-        path: '4',
-        label: 'test',
+        path: 'usersignin',
+        label: 'usersignin',
         element: <CustomerSignin />,
       },
     ],
   },
   {
     id: 60,
-    path: '/11',
+    path: '/dashboard',
     label: 'mypagelayout',
     element: <MypageLayout />,
     withAuth: false,
     children: [
       {
         id: 61,
-        path: '1',
-        label: 'test',
+        path: 'seller',
+        label: 'sellerdashboard',
         element: <OwnerDashboard />,
       },
       {
         id: 62,
-        path: '2',
-        label: 'test',
+        path: 'user',
+        label: 'userdashboard',
         element: <Mypage />,
       },
     ],
+  },
+  {
+    id: 70,
+    path: '/selectedsignup',
+    label: 'selectedsignup',
+    element: <SignInSelectionPage />,
+    withAuth: false,
   },
 ]
 
