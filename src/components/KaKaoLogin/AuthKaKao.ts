1437832f84f6fb4e9e0a254d09de2d57dc283527
swp91 <seongwoo4477@gmail.com>
2023-09-18 14:11:09 +0900

    fix: 카카오로그인 리다이렉트 수정

--- src/components/KaKaoLogin/AuthKaKao.ts
+++ src/components/KaKaoLogin/AuthKaKao.ts
@@ -1,5 +1,5 @@
 const CLIENT_ID = 'd1d67d3d0c029ec63bff82955f1f5976'
-const REDIRECT = 'http://localhost:5173/auth/kakao/callback'
+const REDIRECT = 'http://54.180.103.214:8080/auth/kakao/callback'
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT}&response_type=code`
 // export const KAKAO_AUTH_URL = `kauth.kakao.com/oauth/authorize?client_id=d1d67d3d0c029ec63bff82955f1f5976&redirect_uri=http://54.180.103.214:8080/auth/kakao/callback&response_type=code`
