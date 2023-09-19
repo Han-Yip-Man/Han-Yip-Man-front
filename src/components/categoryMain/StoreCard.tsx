08552356f6e994f3fe9202906671cedd74f9cc45
ckwlghks123 <ckwlghks12+github2@gmail.com>
2023-09-20 06:46:51 +0900

    fix: lint 오류 수정

--- src/components/categoryMain/StoreCard.tsx
+++ src/components/categoryMain/StoreCard.tsx
@@ -16,56 +16,59 @@ function areEqual(prevProps: any, nextProps: any) {
   )
 }
 
-function StoreCard({
-  avgRating,
-  minOrderPrice,
-  reviewCount,
-  shopName,
-  shopDescription,
-  deliveryTime,
-  deliveryPrice,
-}: Props) {
-  return (
-    <S.CustomCard>
-      <S.CustomCardAction>
-        <S.MediaWrap>
-          <S.Media image="/img/jjajang.jpg" />
-        </S.MediaWrap>
-        <S.Content>
-          <S.TitleWrap>
-            <S.Title gutterBottom variant="h5">
-              {shopName}
-            </S.Title>
-            <S.ChipWrap>
-              <span>
-                <S.CustomChip
-                  color="warning"
-                  label={`⭐ ${!avgRating ? '평점없음' : avgRating.toFixed(1)}`}
-                />
-              </span>
-            </S.ChipWrap>
-          </S.TitleWrap>
-          <S.DescWrap>
-            <S.StoreDesc variant="body2" color="GrayText">
-              {shopDescription}
-            </S.StoreDesc>
+const StoreCard = memo(
+  ({
+    avgRating,
+    minOrderPrice,
+    reviewCount,
+    shopName,
+    shopDescription,
+    deliveryTime,
+    deliveryPrice,
+  }: Props) => {
+    return (
+      <S.CustomCard>
+        <S.CustomCardAction>
+          <S.MediaWrap>
+            <S.Media image="/img/jjajang.jpg" />
+          </S.MediaWrap>
+          <S.Content>
+            <S.TitleWrap>
+              <S.Title gutterBottom variant="h5">
+                {shopName}
+              </S.Title>
+              <S.ChipWrap>
+                <span>
+                  <S.CustomChip
+                    color="warning"
+                    label={`⭐ ${!avgRating ? '평점없음' : avgRating.toFixed(1)}`}
+                  />
+                </span>
+              </S.ChipWrap>
+            </S.TitleWrap>
+            <S.DescWrap>
+              <S.StoreDesc variant="body2" color="GrayText">
+                {shopDescription}
+              </S.StoreDesc>
 
-            <S.StoreInfoWrap>
-              <S.ReviewCount color="salmon">리뷰 {reviewCount}</S.ReviewCount>
-              <S.Estimated>예상 배달시간 {deliveryTime / 1000}분</S.Estimated>
-            </S.StoreInfoWrap>
-          </S.DescWrap>
-          <Divider />
-          <S.DeliveryInfo>
-            <S.MinOrder>최소주문금액 {minOrderPrice.toLocaleString()}원</S.MinOrder>
-            <S.DeliveryFee color="slategrey">
-              배달료 {deliveryPrice.toLocaleString()}원
-            </S.DeliveryFee>
-          </S.DeliveryInfo>
-        </S.Content>
-      </S.CustomCardAction>
-    </S.CustomCard>
-  )
-}
+              <S.StoreInfoWrap>
+                <S.ReviewCount color="salmon">리뷰 {reviewCount}</S.ReviewCount>
+                <S.Estimated>예상 배달시간 {deliveryTime / 1000}분</S.Estimated>
+              </S.StoreInfoWrap>
+            </S.DescWrap>
+            <Divider />
+            <S.DeliveryInfo>
+              <S.MinOrder>최소주문금액 {minOrderPrice.toLocaleString()}원</S.MinOrder>
+              <S.DeliveryFee color="slategrey">
+                배달료 {deliveryPrice.toLocaleString()}원
+              </S.DeliveryFee>
+            </S.DeliveryInfo>
+          </S.Content>
+        </S.CustomCardAction>
+      </S.CustomCard>
+    )
+  },
+  areEqual,
+)
 
-export default memo(StoreCard, areEqual)
+export default StoreCard
