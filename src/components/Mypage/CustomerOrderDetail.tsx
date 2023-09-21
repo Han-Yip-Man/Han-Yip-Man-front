14afd5af333ed2a3ceb175a39b44dde5f514a688
seod4423 <seod4423@gmail.com>
2023-09-21 23:09:48 +0900

    fix: toast message, style, useMutation

--- src/components/Mypage/CustomerOrderDetail.tsx
+++ src/components/Mypage/CustomerOrderDetail.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Card, CardContent, Chip, Stack, Typography, styled } from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Chip,
+  Paper,
+  Stack,
+  Typography,
+  styled,
+} from '@mui/material'
 import { DeliveryKakaoMap } from '../../api/kakao.api'
 import { ReviewCardForm } from './ReviewCardForm'
 import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded'
@@ -48,12 +58,14 @@ export const CustomerOrderDetail = ({
         <Typography variant="h5" component={Box}>
           주문 정보
         </Typography>
-        <Typography variant="h6">주문번호: {data?.orderUid}</Typography>
-        <Typography variant="h6">주문시간: {data?.createdAt}</Typography>
-        <Typography variant="h6">주소지: {data?.address}</Typography>
-        <Typography variant="h6">구매자 전화번호: {data?.phoneNum}</Typography>
-        <Typography variant="h6">상호명: {data?.shopName}</Typography>
-        <Typography variant="h6">가게 전화번호: {data?.shopTelphoneNum}</Typography>
+        <OrderInfoPaper>
+          <Typography variant="h6">주문번호: {data?.orderUid}</Typography>
+          <Typography variant="h6">주문시간: {data?.createdAt}</Typography>
+          <Typography variant="h6">주소지: {data?.address}</Typography>
+          <Typography variant="h6">구매자 전화번호: {data?.phoneNum}</Typography>
+          <Typography variant="h6">상호명: {data?.shopName}</Typography>
+          <Typography variant="h6">가게 전화번호: {data?.shopTelphoneNum}</Typography>
+        </OrderInfoPaper>
       </OrderInfoStack>
 
       <DeliveryMapStack>
@@ -81,7 +93,7 @@ export const CustomerOrderDetail = ({
         <Typography variant="h5" component={Box}>
           주문 내역
           {data?.orderMenus.map((menu: any) => (
-            <Card key={menu.name}>
+            <OrderListCard key={menu.name}>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   {data?.shopName}
@@ -93,7 +105,7 @@ export const CustomerOrderDetail = ({
                 ))}
                 <Typography>주문가격 : {menu.price}원</Typography>
               </CardContent>
-            </Card>
+            </OrderListCard>
           ))}
         </Typography>
       </OrderListInfoStack>
@@ -101,24 +113,26 @@ export const CustomerOrderDetail = ({
       <PaymentsStack>
         <Typography variant="h5" component={Box}>
           결제 정보
-          <Typography variant="h6">주문상태: {data?.orderStatus}</Typography>
-          <Typography variant="h6">결제방법: {data?.payMethod}</Typography>
-          {data?.canceledAt ? (
-            <Typography variant="h6">취소시간: {data?.canceledAt}</Typography>
-          ) : null}
-          <Typography variant="h6" component={Box}>
-            결제비용컴포넌트
-            <Typography variant="h6">총 금액: {data?.totalPrice}</Typography>
-            <Typography variant="h6">배달비: {data?.defaultDeliveryPrice}</Typography>
-            <Typography variant="h6">쿠폰할인: {data?.buyerCouponDiscount}</Typography>
-            <hr />
-            <Typography variant="h6">
-              총 결제금액:{' '}
-              {data
-                ? data?.totalPrice + data?.defaultDeliveryPrice - data?.buyerCouponDiscount
-                : null}
+          <PaymentInfoPaper>
+            <Typography variant="h6">주문상태: {data?.orderStatus}</Typography>
+            <Typography variant="h6">결제방법: {data?.payMethod}</Typography>
+            {data?.canceledAt ? (
+              <Typography variant="h6">취소시간: {data?.canceledAt}</Typography>
+            ) : null}
+            <Typography variant="h6" component={Box}>
+              결제비용컴포넌트
+              <Typography variant="h6">총 금액: {data?.totalPrice}</Typography>
+              <Typography variant="h6">배달비: {data?.defaultDeliveryPrice}</Typography>
+              <Typography variant="h6">쿠폰할인: {data?.buyerCouponDiscount}</Typography>
+              <hr />
+              <Typography variant="h6">
+                총 결제금액:{' '}
+                {data
+                  ? data?.totalPrice + data?.defaultDeliveryPrice - data?.buyerCouponDiscount
+                  : null}
+              </Typography>
             </Typography>
-          </Typography>
+          </PaymentInfoPaper>
         </Typography>
       </PaymentsStack>
 
@@ -138,6 +152,13 @@ const OrderDetailWrap = styled(Stack)`
 
 const OrderInfoStack = styled(Stack)`
   margin: 16px 0;
+  > div.MuiTypography-root {
+    margin-top: 30px;
+  }
+`
+
+const OrderInfoPaper = styled(Paper)`
+  padding: 16px;
 `
 
 const DeliveryMapStack = styled(Stack)`
@@ -148,8 +169,16 @@ const PaymentsStack = styled(Stack)`
   margin: 16px 0;
 `
 
+const PaymentInfoPaper = styled(Paper)`
+  padding: 16px;
+`
+
 const OrderListInfoStack = styled(Stack)`
   margin: 16px 0;
+`
+
+const OrderListCard = styled(Card)`
+  margin-top: 20px;
 `
 
 const ReviwFormStack = styled(Stack)`
