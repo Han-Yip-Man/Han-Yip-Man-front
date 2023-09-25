75e8fa4eab821ab18e8335f83dc653913e34d0b3
swp91 <seongwoo4477@gmail.com>
2023-09-25 20:52:12 +0900

    feat: 테스트

--- src/atoms/index.tsx
+++ src/atoms/index.tsx
@@ -1,6 +1,11 @@
-import { atom } from 'recoil'
-
-export const jeeinAtom = atom({
-  key: 'jeein',
-  default: true,
-})
+export { persistAtom, curretNonAddr, userAddr, currentAddr } from './addressAtoms.ts'
+export { CartStateAtom, cartSelectors, totalCartPriceSelector } from './cartAtoms.ts'
+export { endPointLocationAtom } from './deliveryAtoms.ts'
+export { keyword, focusState, dataState } from './mainAtoms.ts'
+export {
+  UserStateAtom,
+  CouponDiscountAtom,
+  FinalChargePriceAtom,
+  orderShopid,
+} from './orderAtoms.ts'
+export {} from './orderManageAtoms.ts'
